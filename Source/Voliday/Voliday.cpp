768b925c91874b57939b9690091cae8defd38bca
Oleg Loshkin <loshkin.oleg.95@gmail.com>
2022-05-28 09:44:55 +0200

    clearing the cache

--- Source/Voliday/Voliday.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Voliday.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Voliday, "Voliday" );
-
-DEFINE_LOG_CATEGORY(LogVoliday)
- 
